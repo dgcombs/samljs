00c68673c2fd384016427ec0e4fb9601fdb7d2cb
Dan McGinn-Combs <dgcombs@gmail.com>
2011-08-05 17:30:51 -0400

    Added DES

--- js/genSAML.js
+++ js/genSAML.js
@@ -4,37 +4,33 @@
 
 /*
 **
-** SAML Assertion Format
-
-* XML Declaration
-
-*samlp:Response
-
-**saml:Issuer
-
-**samlp:Status
-
-	***samlp:StatusCode
-
-**saml:Assertion
-***saml:Issuer
-***saml:Subject
-****saml:NameID
-****saml:SubjectConfirmation
-*****saml:SubjectConfirmationData
-***saml:Conditions
-****saml:AudienceRestriction
-*****saml:Audience
-***saml:AuthnStatement
-****saml:AuthnContext
-*****samlAuthnContextClassRef
-***saml:AttributeStatement
-****saml:Attribute
-*****saml:AttributeValue
+** SAML 2.0 Assertion Format
+** !!! Caution !!!
+** The information below is my understanding of a SAML Assertion format:
+** ---------------
+** 1) the Response section - the primary container for idp communication to sp
+**	Inside the Response are Issuer and Status and inside Status is Status Code
+** 2) the Assertion Section contains the several key bits of information
+**	The Issuer (again)
+**	The Subject (including NameID, Subject Confirmation and Subject Confirmation Data)
+**	The Conditions of the Assertion including Audience Restriction and Audience
+**	The AuthnStatement with AuthnContext and AuthnContextClassRef
+**	The AttributeStatement with Attribute and AttributeValue
+** 3) the Signature section
+**	Contains information about the security used to sign the Assertion
+** (The code for this section is still pending)
 **
+**	The standard XML Declaration is not required/mandated
+**	<?xml version="1.0" encoding="UTF-8"?>
+** 
+** Because of this hierarchy, the code below is written as a series of embeddable
+** functions. No attempt at streamlining, optimization or execution is attempted.
+** The goals include simplicity, clarity and accessiblity for those attempting to use
+** this code to test SAML-based systems.
 */
 var thisInstant;
 var nextInstant;
+var key="JpKJLO2qaMkgEs4VFYEX+eYnn0J6LXXI"; //DES Key
 
 function createIssuer() {
 	// Create the XML expression for the Issuer section
@@ -61,11 +57,45 @@ function createConditions() {
 }
 
 function createAssertion() {
-	return element("saml:Assertion",createSubject() + createIssuer() + createConditions() + createAuthnStatement(),{"xmlns:saml":"urn:oasis:names:tc:SAML:2.0:assertion","ID":document.SAMLForm.assertionID.value,"Version":"2.0"});
+	/*
+	** Based on the variable document.SAMLForm.Encrypt.value
+	** Choose an encryption type for Assertion contents
+	** <element name="EncryptedAssertion" type="saml:EncryptedElementType"/>
+	*/
+	var assertionContents = createSubject() + createIssuer() + createConditions() + createAuthnStatement();
+	if (document.SAMLForm.encryptQ.value == "assertion") {
+		switch (document.SAMLForm.Encrypt.value) {
+			case "DES":
+				var des = new DES(key, "");
+				return element("saml:EncryptedAssertion",Base64.encode(des.encrypt(assertionContents)),{"xmlns:saml":"urn:oasis:names:tc:SAML:2.0:assertion","ID":document.SAMLForm.assertionID.value,"Version":"2.0"});
+				break;
+			case "AES":
+			case "TEA":
+			default:
+				return element("saml:Assertion",assertionContents,{"xmlns:saml":"urn:oasis:names:tc:SAML:2.0:assertion","ID":document.SAMLForm.assertionID.value,"Version":"2.0"});
+				break;
+		}
+	} else {
+		return element("saml:Assertion",assertionContents,{"xmlns:saml":"urn:oasis:names:tc:SAML:2.0:assertion","ID":document.SAMLForm.assertionID.value,"Version":"2.0"});
+	}
 }
 
 function createSAML() {
-	return element("samlp:Response",createAssertion(),{"xmlns:samlp":"urn:oasis:names:tc:SAL:2.0:protocol","ID":"Some Big Number","IssueInstant":thisInstant,"Version":"2.0"});
+	if document.SAMLForm.encryptQ.value == "response") {
+		switch (document.SAMLForm.Encrypt.value) {
+			case "DES":
+				var des = new DES(key,"");
+				return element("samlp:Response",Base64.encode(des.encrypt(createAssertion()),{"xmlns:samlp":"urn:oasis:names:tc:SAL:2.0:protocol","ID":"Some Big Number","IssueInstant":thisInstant,"Version":"2.0"});
+				break;
+			case "AES":
+			case "TEA":
+			default:
+				return element("samlp:Response",createAssertion(),{"xmlns:samlp":"urn:oasis:names:tc:SAL:2.0:protocol","ID":"Some Big Number","IssueInstant":thisInstant,"Version":"2.0"});
+				break;
+		}
+	} else {
+		return element("samlp:Response",createAssertion(),{"xmlns:samlp":"urn:oasis:names:tc:SAL:2.0:protocol","ID":"Some Big Number","IssueInstant":thisInstant,"Version":"2.0"});
+	}
 }
 
 function submit_form() {
@@ -76,26 +106,6 @@ function submit_form() {
 	var nextInstant=d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate() + "T" + d.getHours() + ":" + d.getMinutes() + ":00Z";
 	// set Action for URL
 	document.SAMLForm.action=document.SAMLForm.targetID.value;
-	var SAMLResponse = createSAML();
-	// alert(SAMLResponse);
-	switch (document.SAMLForm.Encrypt.value) {
-		case "DES":
-			var myIV = "";
-			var key="JpKJLO2qaMkgEs4VFYEX+eYnn0J6LXXI";
-			var des = new DES(key,myIV);
-			SAMLResponse=des.encrypt(SAMLResponse)
-			SAMLResponse=Base64.encode(SAMLResponse);
-			console.log("SAMLResponse is : " + SAMLResponse);
-			document.SAMLForm.SAMLResponse.value=SAMLResponse;
-			break;
-		case "3DES":
-			break;
-		case "TEA":
-			break;
-		case "AES":
-			break;
-		default:
-	}
-	document.SAMLForm.SAMLResponse.value=SAMLResponse;
+	document.SAMLForm.SAMLResponse.value = createSAML();
 	document.SAMLForm.submit();
 }
